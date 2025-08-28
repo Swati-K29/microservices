cb384c2518e2f86804b50d97379a551eb140a282
Swati-K29 <swatikadu29.sk@gmail.com>
2025-08-29 03:07:06 +0530

    API documentation added

--- src/main/java/com/practise/accounts/AccountsApplication.java
+++ src/main/java/com/practise/accounts/AccountsApplication.java
@@ -1,11 +1,39 @@
 package com.practise.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Udemy course on microservices - Account microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Swati Kadu",
+                        email = "swatikadu29.sk@gmail.com",
+                        url = "url"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "url"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+            description = "Udemy course Accountss microservice REST API Docummentation",
+            url = "url"
+    )
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
