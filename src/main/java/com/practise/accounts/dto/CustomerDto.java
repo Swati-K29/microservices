e2062e7f3ed1c31400e8fc05bbaaef841d3fdb31
Swati-K29 <swatikadu29.sk@gmail.com>
2025-08-28 18:30:35 +0530

    DTOs and mapper created for Accounts, Customer

---
+++ src/main/java/com/practise/accounts/dto/CustomerDto.java
@@ -0,0 +1,13 @@
+package com.practise.accounts.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
---
+++ src/main/java/com/practise/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,21 @@
+package com.practise.accounts.mapper;
+
+import com.practise.accounts.dto.CustomerDto;
+import com.practise.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto){
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer){
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
