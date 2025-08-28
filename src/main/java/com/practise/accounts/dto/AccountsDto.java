ff84b79c0df8fa399f7949a37ce4f2cc8a91bfdb
Swati-K29 <swatikadu29.sk@gmail.com>
2025-08-29 01:34:48 +0530

    Data validation- on requestbody data- dto

--- src/main/java/com/practise/accounts/dto/AccountsDto.java
+++ src/main/java/com/practise/accounts/dto/AccountsDto.java
@@ -2,14 +2,20 @@ package com.practise.accounts.dto;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type cannot be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address cannot be null or empty")
     private String branchAddress;
 }
