6277e10d24429970e974d3a49e9d925af69d4a21
Swati-K29 <swatikadu29.sk@gmail.com>
2025-08-29 13:15:39 +0530

    cards service skeleton project added

---
+++ cards/src/test/java/com/practise/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
