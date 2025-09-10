f2d9a28303b818ca581935899cb7831e80431b01
Swati-K29 <swatikadu29.sk@gmail.com>
2025-09-10 21:44:05 +0530

    read, update, delete methods implemented

--- loans/src/main/java/com/practise/loans/controller/LoansController.java
+++ loans/src/main/java/com/practise/loans/controller/LoansController.java
@@ -1,16 +1,14 @@
 package com.practise.loans.controller;
 
 import com.practise.loans.constants.LoansConstants;
+import com.practise.loans.dto.LoansDto;
 import com.practise.loans.dto.ResponseDto;
 import com.practise.loans.service.ILoansService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path ="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
@@ -27,4 +25,37 @@ public class LoansController {
                 .body(new ResponseDto(LoansConstants.STATUS_201, LoansConstants.MESSAGE_201));
     }
 
+    @GetMapping("/fetch")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber){
+       LoansDto loansDto = iLoansService.fetchLoan(mobileNumber);
+       return ResponseEntity.status(HttpStatus.OK).body(loansDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateLoanDetails(@RequestBody LoansDto loansDto){
+        boolean isUpdated = iLoansService.updateLoan(loansDto);
+        if(isUpdated){
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(LoansConstants.STATUS_200,LoansConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417,LoansConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteLoanDetails(@RequestParam String mobileNumber){
+        boolean isDeleted = iLoansService.deleteLoan(mobileNumber);
+        if(isDeleted){
+          return ResponseEntity
+                  .status(HttpStatus.OK)
+                  .body(new ResponseDto(LoansConstants.STATUS_200,LoansConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(LoansConstants.STATUS_417,LoansConstants.MESSAGE_417_DELETE));
+        }
+    }
 }
--- loans/src/main/java/com/practise/loans/service/ILoansService.java
+++ loans/src/main/java/com/practise/loans/service/ILoansService.java
@@ -1,9 +1,17 @@
 package com.practise.loans.service;
 
+import com.practise.loans.dto.LoansDto;
+
 public interface ILoansService {
     /**
      *
      * @param mobileNumber - Mobile number of the customer
      */
     void createLoans(String mobileNumber);
+
+    LoansDto fetchLoan(String mobileNumber);
+
+    boolean updateLoan(LoansDto loansDto);
+
+    boolean deleteLoan(String mobileNumber);
 }
